61cc64f25ff0cac7039474f993358c0f438de967
CarrotonMan <168975034+CarrotonMan@users.noreply.github.com>
2024-05-08 19:02:20 -0400

    Add files via upload

--- BalatroDarkMode/BalatroDarkMode.lua
+++ BalatroDarkMode/BalatroDarkMode.lua
@@ -15,14 +15,30 @@ function SMODS.INIT.Colors()
         Clubs = HEX("28aa1a"),
     }
 
+    G.C.SO_1 = {
+        Hearts = HEX('f20000'),
+        Diamonds = HEX('cb0b83'),
+        Spades = HEX("4f31b9"),
+        Clubs = HEX("28aa1a"),
+    }
+
+    G.C.BACKGROUND = {
+        L = {1,1,0,0},
+        D = HEX("374244"),
+        C = HEX("374244"),
+        contrast = 1
+    }
+
     local darkmode_mod = SMODS.findModByID("BalatroDarkMode")
     local dark_enhancer = SMODS.Sprite:new("centers", darkmode_mod.path, "DarkEnhancers.png", 71, 95, "asset_atli")
     local deck_standard = SMODS.Sprite:new("cards_1", darkmode_mod.path, "DarkDeck.png", 71, 95, "asset_atli")
     local ui_standard = SMODS.Sprite:new("ui_1", darkmode_mod.path, "DarkUI.png", 18, 18, "asset_atli")
+    local dark_balatro = SMODS.Sprite:new("balatro", darkmode_mod.path, "DarkBalatro.png", 333, 216, "asset_atli")
     
     dark_enhancer:register()
     deck_standard:register()
     ui_standard:register()
+    dark_balatro:register()
 end
 
 ----------------------------------------------
